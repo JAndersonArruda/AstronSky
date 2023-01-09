d646cbb4bcad1607e4f55b2624e915f8d34deebe
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2023-01-09 13:46:45 -0300

    alteração nas routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 const Stack = createNativeStackNavigator();
+// const Tab = createBottomTabNavigator();
 
 import Preload from '../pages/Preload';
 import SigIn from '../pages/SigIn';
 import SigUp from '../pages/SigUp';
 import Home from '../pages/Home';
-import News from '../pages/News';
 import Galerie from '../pages/Galerie';
+import News from '../pages/News';
 import Profile from '../pages/Profile';
 import EditProfile from '../pages/EditProfile';
 
+
+// const MainTab = () => {
+//     <Tab.Navigator initialRouteName='Home'>
+//         <Tab.Screen name='Home' component={Home} />
+//         <Tab.Screen name='Galerie' component={Galerie} />
+//     </Tab.Navigator>
+// }
 
 const AppRoutes = () => {
     return (
@@ -23,10 +32,7 @@ const AppRoutes = () => {
                 <Stack.Screen name='SigIn' component={SigIn} />
                 <Stack.Screen name='SigUp' component={SigUp} />
                 <Stack.Screen name='Home' component={Home} />
-                <Stack.Screen name='News' component={News} />
                 <Stack.Screen name='Galerie' component={Galerie} />
-                <Stack.Screen name='Profile' component={Profile} />
-                <Stack.Screen name='EditProfile' component={EditProfile} />
             </Stack.Navigator>
         </NavigationContainer>
     )
